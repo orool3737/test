b9d8f1b6ebfc4788be984e789df35dd5a46e3e11
orool3737 <orool3737@naver.com>
2021-01-31 21:53:35 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import function
+
 a = []
 a.append("뚤기" + "슬기" + "메롱")
 print(a[0])
@@ -5,5 +7,5 @@ a.append("웅이는 " + "완전 " + "짱짱맨")
 print(a[1])
 print('\n'.join(a))
 c=1
-b =func(c)
+b = func(c)
 print(b)
