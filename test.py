57b9264fb9572906325dde14db379ac6bb6ed531
orool3737 <orool3737@naver.com>
2021-03-11 16:04:28 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+latest = "21/03/11"
+
 with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
     json.dump(latest, json_file, ensure_ascii = False)
