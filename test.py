0216222e4df19140a18edd947280e396ffe839a5
orool3737 <orool3737@naver.com>
2021-03-11 16:00:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,4 @@
-from function import func
+import json
 
-a = []
-a.append("뚤기" + "슬기" + "메롱")
-print(a[0])
-a.append("웅이는 " + "완전 " + "짱짱맨")
-print(a[1])
-print('\n'.join(a))
-c=1
-b = func(c)
-print(str(b))
-
-price = "96,000"
-print(int(price.replace(',', '')))
+with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
+    json.dump(latest, json_file, ensure_ascii = False)
